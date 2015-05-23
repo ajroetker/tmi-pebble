f6e3d9dd187f9bd13737ed5414bf45a44c5425f3
Andrew Roetker <andrew.roetker@puppetlabs.com>
2015-05-23 09:25:39 -0700

    part 3 step 1

--- src/Tutorial1.c
+++ src/Tutorial1.c
@@ -7,6 +7,9 @@ static GFont s_time_font;
 static BitmapLayer *s_background_layer;
 static GBitmap *s_background_bitmap;
 
+static TextLayer *s_time_layer
+static TextLayer *s_weather_layer;
+
 static void update_time () {
   // Get a tm structure
   time_t temp = time (NULL);
@@ -53,6 +56,18 @@ static void main_window_load (Window *window) {
   
   // Make sure the time is displayed from the start
   update_time ();
+  
+  // Create temperature Layer
+  s_weather_layer = text_layer_create (GRect (0, 130, 144, 25));
+  text_layer_set_background_color (s_weather_layer, GColorClear);
+  text_layer_set_text_color (s_weather_layer, GColorWhite);
+  text_layer_set_text_alignment (s_weather_layer, GTextAlignmentCenter);
+  text_layer_set_text (s_weather_layer, "Loading...");
+
+  // Create second custom font, apply it and add to Window
+  s_weather_font = fonts_load_custom_font (resource_get_handle (RESOURCE_ID_FONT_PERFECT_DOS_20));
+  text_layer_set_font (s_weather_layer, s_weather_font);
+  layer_add_child (window_get_root_layer (window), text_layer_get_layer (s_weather_layer));
 }
 
 static void main_window_unload (Window *window) {
@@ -64,6 +79,10 @@ static void main_window_unload (Window *window) {
   fonts_unload_custom_font (s_time_font);
   // Destroy TextLayer
   text_layer_destroy (s_time_layer);
+
+  // Destroy weather elements
+  text_layer_destroy (s_weather_layer);
+  fonts_unload_custom_font (s_weather_font);
 }
 
 static void tick_handler (struct tm *tick_time, TimeUnits units_changed) {
