5bdb5580af3cdd1d69726b99e6bfe61b782476ef
Andrew Roetker <andrew.roetker@puppetlabs.com>
2015-05-23 00:13:57 -0700

    part 2 step 1

--- src/Tutorial1.c
+++ src/Tutorial1.c
@@ -2,6 +2,7 @@
 
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+static GFont s_time_font;
 
 static void update_time () {
   // Get a tm structure
@@ -26,19 +27,27 @@ static void update_time () {
 
 static void main_window_load (Window *window) {
   // Create time TextLayer
-  s_time_layer = text_layer_create (GRect (0, 55, 144, 50));
+  s_time_layer = text_layer_create (GRect (5, 52, 139, 50));
   text_layer_set_background_color (s_time_layer, GColorClear);
   text_layer_set_text_color (s_time_layer, GColorBlack);
 
-  // Improve the layout to be more like a watchface
-  text_layer_set_font (s_time_layer, fonts_get_system_font (FONT_KEY_BITHAM_42_BOLD));
+  // Create GFont
+  s_time_font = fonts_load_custom_font (resource_get_handle (RESOURCE_ID_FONT_PERFECT_DOS_48));
+
+  // Apply to TextLayer
+  text_layer_set_font (s_time_layer, s_time_font);
   text_layer_set_text_alignment (s_time_layer, GTextAlignmentCenter);
 
   // Add it as a child layer to the Window's root layer
   layer_add_child (window_get_root_layer (window), text_layer_get_layer (s_time_layer));
+  
+  // Make sure the time is displayed from the start
+  update_time ();
 }
 
 static void main_window_unload (Window *window) {
+  // Unload GFont
+  fonts_unload_custom_font (s_time_font);
   // Destroy TextLayer
   text_layer_destroy (s_time_layer);
 }
@@ -59,9 +68,6 @@ static void init () {
 
   // Show the Window on the watch, with animated=true
   window_stack_push (s_main_window, true);
-  
-  // Make sure the time is displayed from the start
-  update_time ();
   
   // Register with TickTimerService
   tick_timer_service_subscribe (MINUTE_UNIT, tick_handler);
